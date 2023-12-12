e7408f5ded2322e7ad679bf17205fcc228ce79fa
dany002 <moldovandangelu@gmail.com>
2023-12-12 13:41:11 +0200

    initial commit

--- LAB5/main.py
+++ LAB5/main.py
@@ -177,19 +177,18 @@ if __name__ == '__main__':
     grammar1 = Grammar("grammar1.txt")
     grammar2 = Grammar("grammar2.txt")
 
+    grammar = Grammar("gram.txt")
+    grammar.print_nonterminals()
+    grammar.print_terminals()
+    grammar.print_productions()
+    grammar.print_productions_for_nonterminal('iostmt')
+
     parser1 = Parser(grammar1)
     parser2 = Parser(grammar2)
-
+    #
     parser1.add_productions_to_file("( int + int )")
     parser2.add_productions_to_file("< and or and >")
-
-    # print()
-    # parser1.run_tests()
-    # grammar1.print_s()
-    # grammar1.print_terminals()
-    # grammar1.print_productions()
-    # grammar1.print_nonterminals()
-    # grammar1.print_productions_for_nonterminal("A")
+    #
     parser1.parser_output.print_to_screen()
     parser1.parser_output.print_to_file("out1.txt")
     parser2.parser_output.print_to_screen()
